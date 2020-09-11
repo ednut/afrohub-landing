99dcf9f03e0996862f3bab3c3842a57d54b95c7a
Babatunde <babatundeadegbite@gmail.com>
2020-09-11 22:57:59 +0100

    meta tags fix

--- components/Meta.js
+++ components/Meta.js
@@ -4,12 +4,35 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+    <title>AfroHub</title>
+    <meta name="description" content="Unifying the global Black network: AfroHub is a space for black community to foster innovation, collaborate, and network in tech to grow and build black-led products."/>
+    <meta name="keywords" content="Africans, Blacks, Tech, Afro, Hub, AfroHub, Technology, Products, Community, Collaborate, Network, Connect, Discover, Culture, Nigeria, Ghana, America, South Africa, Canada, Liberia, Senegal, Kenya, Gabon"/>
+    <meta name="author" content="AfroHub"/>
+    <link rel="canonical" href="https://www.afrohub.co"/>
+    <meta name="robots" content="index"/>
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png"></link>
     <link rel="shortcut icon" href="/static/favicon.png" />
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-    <title>AfroHub</title>
+    <meta property="og:url" content="https://www.afrohub.co"/>
+    <meta property='og:image' content="https://res.cloudinary.com/afrohub/image/upload/v1595426186/Assets/images/afrohub_lzgz4f.png" />
+    <meta property="og:image:width" content="500" />
+    <meta property="og:image:height" content="500" />
+    <meta property="og:type" content="website" />
+    <meta property='og:title' content="AfroHub | Black Tech Community"/>
+    <meta property='og:site_name' content="AfroHub"/>
+    <meta property="og:description" content="Unifying the global Black network: AfroHub is a space for black community to foster innovation, collaborate, and network in tech to grow and build black-led products." />
   </Head>
 );
 
 export default Meta;
+
+
+
+
+
+
+
